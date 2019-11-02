8c7de49cfb126be4e23700ec1f0b86e9fff761a1
Prateek Vachher <vachh007@umn.edu>
2019-11-01 20:53:21 -0500

    added

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
 from flask import Flask,render_template, request
+from textblob import TextBlob
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=["GET", "POST"])
-def login():
-    error = ""
+def home():
+    sentiment, count, positive, negative = 0, 0, 0, 0
+    file_load = open("reviews.csv", "r")
+    x = file_load.readlines()
+    db = []
+    for y in x:
+        y = y.split(",")
+        if len(y) > 5 and len(y[5]) > 4:
+            line = (" ".join(y[5:]))
+            wiki = TextBlob(line)
+            count += 1
+            sentiment += wiki.sentiment.polarity
+            if wiki.sentiment.polarity > 0:
+                positive  += 1
+            else:
+                negative += 1
+            entry = {}
+            entry["name"] = y[1]
+            entry["sentiment"] = round(wiki.sentiment.polarity,2)
+            entry["comment"] = line
+            db.append(entry)
+    average_sentiment = round(sentiment/count,2)
+    return render_template('index.html', sentiment=average_sentiment, positive=positive,negative=negative,count=count, list_reviews=db[1:], posper = round(positive/count * 100, 2), negper =  round(negative/count*100, 2))
 
-    if request.method == "POST":
-        if request.form['username'] == "sifat" and request.form['password'] == "khalsa":
-            return render_template('add_feedback.html', payload={})
-        else:
-            error = "Invalid Username/Password"
-
-    return render_template('login.html', error = error)
-
-@app.route('/student_portal')
-def info():
-    return render_template('add_feedback.html', payload={})
-
-
-@app.route('/student/<name>')
-def data(name):
-    return "<h1> {} can use this personalized portal. </h1>".format(name)
-
-
-@app.route('/employee_portal')
-def rec():
-    return "<h1> Employee recieve data over here </h1>"
-
-@app.route('/thank')
-def thanks():
-    return render_template('thank.html')
 
 if __name__== '__main__':
     app.run(debug=True) 
